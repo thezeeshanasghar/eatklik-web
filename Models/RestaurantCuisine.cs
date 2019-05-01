26ef83cb2439da35aa77f73e553ffcbb3ef8fb60
thezeeshanasghar <thezeeshanasghar@gmail.com>
2019-05-01 13:16:40 +0500

    add json ignore

--- Models/RestaurantCuisine.cs
+++ Models/RestaurantCuisine.cs
@@ -1,5 +1,6 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace eatklik.Models
 {
@@ -7,8 +8,10 @@ namespace eatklik.Models
     public class RestaurantCuisine
     {
         public long RestaurantId { get; set; }
+        [JsonIgnore]
         public Restaurant Restaurant { get; set; }
         public long CuisineId { get; set; }
+        [JsonIgnore]
         public Cuisine Cuisine { get; set; }
     }
 
