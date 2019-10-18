9be5745cc419a7c4f320e8ec12a3645eadf2d117
thezeeshanasghar <thezeeshanasghar@gmail.com>
2019-10-18 16:57:05 +0500

    add new fields migration

---
+++ Migrations/20191018115644_addFieldsInExtraItem.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eatklik.Migrations
+{
+    public partial class addFieldsInExtraItem : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Size",
+                table: "RestaurantExtraItems",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CoverImagePath",
+                table: "Cities",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Size",
+                table: "RestaurantExtraItems");
+
+            migrationBuilder.DropColumn(
+                name: "CoverImagePath",
+                table: "Cities");
+        }
+    }
+}
