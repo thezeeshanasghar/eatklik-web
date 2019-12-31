45046a88ecb04f198c09863ad15e2944347bc901
theirfanch <chirfan521@gmail.com>
2019-12-31 15:07:13 +0500

    restaurantreviews

---
+++ Migrations/20191231095519_restaurantreviews.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eatklik.Migrations
+{
+    public partial class restaurantreviews : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Cities_CityId",
+                table: "Customers");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "Created",
+                table: "Reviews",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<decimal>(
+                name: "Rating",
+                table: "Restaurants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "reviewCount",
+                table: "Restaurants",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instruction",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PayMethod",
+                table: "Orders",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Size",
+                table: "MenuItems",
+                nullable: true,
+                oldClrType: typeof(int));
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CityId",
+                table: "Customers",
+                nullable: true,
+                oldClrType: typeof(long));
+
+            migrationBuilder.CreateTable(
+                name: "MenuExtraItems",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("MySQL:AutoIncrement", true),
+                    Name = table.Column<string>(nullable: true),
+                    ImagePath = table.Column<string>(nullable: true),
+                    Size = table.Column<string>(nullable: true),
+                    Price = table.Column<long>(nullable: false),
+                    Status = table.Column<int>(nullable: false),
+                    MenuId = table.Column<long>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_MenuExtraItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_MenuExtraItems_Menus_MenuId",
+                        column: x => x.MenuId,
+                        principalTable: "Menus",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_MenuExtraItems_MenuId",
+                table: "MenuExtraItems",
+                column: "MenuId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Cities_CityId",
+                table: "Customers",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Customers_Cities_CityId",
+                table: "Customers");
+
+            migrationBuilder.DropTable(
+                name: "MenuExtraItems");
+
+            migrationBuilder.DropColumn(
+                name: "Created",
+                table: "Reviews");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "reviewCount",
+                table: "Restaurants");
+
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Instruction",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "PayMethod",
+                table: "Orders");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "Size",
+                table: "MenuItems",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<long>(
+                name: "CityId",
+                table: "Customers",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Customers_Cities_CityId",
+                table: "Customers",
+                column: "CityId",
+                principalTable: "Cities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
