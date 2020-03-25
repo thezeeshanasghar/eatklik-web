c5d8c88ecc8eff8aef936158b9fedeffc751f87d
Naeem_Abbas <naeemabbasa25@gmail.com>
2020-03-25 16:40:58 +0500

    OTP

--- Models/UserAuthentication.cs
+++ Models/UserAuthentication.cs
@@ -9,7 +9,8 @@ namespace eatklik.Models
         public long Id { get; set; }
         public string Code { get; set; }
         public string Type { get; set; }
-        public long CustomerId { get; set; } //User May be Customer or Rider
+        public long CustomerId { get; set; } 
+        public long RiderId { get; set; } 
         public int IsVerified { get; set; } // 0 for false , 1 for true
     }
 
